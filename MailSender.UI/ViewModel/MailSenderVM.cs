690360a332cb528703531be76129c6369db5dab8
v.pavlenko <vpavlenko476@gmail.com>
2020-04-24 17:50:17 +0300

    [add] TabControl

--- MailSender.UI/ViewModel/MailSenderVM.cs
+++ MailSender.UI/ViewModel/MailSenderVM.cs
@@ -3,6 +3,7 @@ using GalaSoft.MvvmLight;
 using MailSender.DAL.Consts;
 using MailSender.DAL.Models;
 using MailSender.DAL.Services;
+using System.Configuration;
 using System.Net.Mail;
 using System.Threading.Tasks;
 
@@ -15,6 +16,10 @@ namespace MailSender.UI.ViewModel
         private string _messageBody;
         private string _messageTitle;
         private bool _isBusy;
+        private string login = ConfigurationManager.AppSettings["login"];
+        private string password = ConfigurationManager.AppSettings["password"];
+        private string emailFrom = ConfigurationManager.AppSettings["emailFrom"];
+        private string emailTo = ConfigurationManager.AppSettings["emailTo"];
 
         public MailSenderVM()
         {
@@ -67,9 +72,9 @@ namespace MailSender.UI.ViewModel
             {
                 await _emailSendService.SendEmail(
                 new SmtpClient(Hosts.YandexHost, 25),
-                new Message() { Body = _messageBody, From = "nox1001@yandex.ru", To = "nox1001@yandex.ru", Title = _messageTitle },
-                "nox1001@yandex.ru",
-                "06AA07bb1995cc");
+                new Message() { Body = _messageBody, From = emailFrom, To = emailTo, Title = _messageTitle },
+                login,
+                password);
             }
 
             IsBusy = false;
